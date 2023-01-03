e445697d4547b8b9a3f816c1e1918d4b194a71ab
Prageet05 <prageetsharma05@gmail.com>
2023-01-03 10:27:30 +0530

    final submission

--- src/index.js
+++ src/index.js
@@ -1,18 +1,13 @@
 const express = require('express');
-const route = require('./route/route');
 const mongoose = require('mongoose');
+const route = require('./route/route');
 const app = express();
-app.use(express.json())
-// app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect("mongodb+srv://garimasachan:w2kLOYcomIOBytoJ@blogprojectcluster4.i8kenbl.mongodb.net/blogproject", {
-   useNewUrlParser: true 
-})
+app.use(express.json())
+
+mongoose.connect("mongodb+srv://prageet:4zGItzVIrinYLkAI@myproject.kthcntq.mongodb.net/blogproject")
 .then( () => {console.log( "MongoDb is connected")}  )
 .catch( err => console.log(err))
 
 app.use('/', route);
-
-app.listen(process.env.PORT || 3000, function() {
-    console.log('Express app running on port ' + (process.env.PORT || 3000))
-});
+app.listen(3000, function() {console.log("Express app running on port 3000")})
